b54477263057a205a2c5c161c62688f976b2f991
AK201314 <88940210+AK201314@users.noreply.github.com>
2024-04-12 00:33:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,8 +11,14 @@
 #
 
 # 更改默认IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.50.2/g' package/base-files/files/bin/config_generate
 
 rm -rf feeds/luci/applications/luci-app-openclash
+rm -rf feeds/luci/applications/luci-app-dockerman
 git clone -b master --single-branch --filter=blob:none https://github.com/vernesong/OpenClash.git feeds/luci/applications/luci-app-openclash
 git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
+git clone https://github.com/AK201314/luci-app-temp-status.git
+git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
